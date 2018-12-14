51d13a5ee2433b5826530bfafdc72c5fc40c7023
Chris Ricketts <cricketts497@gmail.com>
2018-12-14 18:05:52 +0100

    Added the addition, subtraction and equals sign, plus the backend calculate function for doing the operations

--- include/calculator.h
+++ include/calculator.h
@@ -18,12 +18,21 @@ private slots:
 	void digitClicked();
 	void pointClicked();
 	void changeSignClicked();
+	void backspaceClicked();
+	void clearClicked();
+	void additiveOperatorClicked();
+	void equalsClicked();
+
+	bool calculate(double operand, const QString &pendingOperator);
+
 
 private:
 	Button *createButton(const QString &text, const char *member);
 
 	double sumInMemory;
+	double sumSoFar;
 	bool waitingForOperand;
+	QString pendingAdditiveOperator;
 
 	QLineEdit *display;
 
--- src/calculator.cpp
+++ src/calculator.cpp
@@ -11,6 +11,7 @@
 Calculator::Calculator(QWidget *parent) : QWidget(parent)
 {
 	sumInMemory = 0.0;
+	sumSoFar = 0.0;
 	waitingForOperand = true;
 
 	//create the widgets
@@ -25,22 +26,36 @@ Calculator::Calculator(QWidget *parent) : QWidget(parent)
 
 	Button *pointButton = createButton(".", SLOT(pointClicked()));
 	Button *changeSignButton = createButton("\302\261", SLOT(changeSignClicked()));
+	Button *backspaceButton = createButton("Backspace", SLOT(backspaceClicked()));
+	Button *clearButton = createButton("Clear", SLOT(clearClicked()));
+
+	Button *addOperatorButton = createButton("+", SLOT(additiveOperatorClicked()));
+	Button *minusOperatorButton = createButton("-", SLOT(additiveOperatorClicked()));
+
+	Button *equalsButton = createButton("=", SLOT(equalsClicked()));
 
 
 	//put the widgets on a grid
 	QGridLayout *mainLayout = new QGridLayout;
 	mainLayout -> setSizeConstraint(QLayout::SetFixedSize);
-	mainLayout -> addWidget(display, 0,0,1,6);
+	mainLayout -> addWidget(display,0,0,1,6);
 
 	for(int i=1; i<NumDigitButtons; ++i){
-		int row = ((9-i)/3) + 2;
+		int row = ((9-i)/3) + 3;
 		int col = (i-1)%3 + 1;
 		mainLayout -> addWidget(digitButtons[i], row, col);
 	}
-	mainLayout -> addWidget(digitButtons[0],5,1);
+	mainLayout -> addWidget(digitButtons[0],6,1);
 
-	mainLayout -> addWidget(pointButton,5,2);
-	mainLayout -> addWidget(changeSignButton,5,3);
+	mainLayout -> addWidget(pointButton,6,2);
+	mainLayout -> addWidget(changeSignButton,6,3);
+	mainLayout -> addWidget(backspaceButton,2,3,1,2);
+	mainLayout -> addWidget(clearButton,2,1,1,2);
+
+	mainLayout -> addWidget(addOperatorButton,6,4);
+	mainLayout -> addWidget(minusOperatorButton,5,4);
+
+	mainLayout -> addWidget(equalsButton, 6,5);
 
 	setLayout(mainLayout);
 	setWindowTitle("Calculator");
@@ -87,9 +102,75 @@ void Calculator::changeSignClicked()
 	display->setText(text);
 }
 
+void Calculator::backspaceClicked()
+{
+	if (waitingForOperand)
+		return;
+
+	QString text = display->text();
+	text.chop(1);
+	if (text.isEmpty()){
+		text = "0";
+		waitingForOperand = true;
+	}
+	display->setText(text);
+}
+
+void Calculator::clearClicked()
+{
+	if (waitingForOperand)
+		return;
+
+	display->setText("0");
+	waitingForOperand = true;
+}
+
+void Calculator::additiveOperatorClicked()
+{
+	Button *clickedButton = qobject_cast<Button *>(sender());
+	QString clickedOperator = clickedButton->text();
+	double operand = display->text().toDouble();
+
+	if (pendingAdditiveOperator.isEmpty()){
+		sumSoFar = operand;	
+	} else {
+		calculate(operand, pendingAdditiveOperator);
+		display->setText(QString::number(sumSoFar));
+	}
+	
+	pendingAdditiveOperator = clickedOperator;
+	waitingForOperand = true;
+}
+
+void Calculator::equalsClicked()
+{
+	double operand = display->text().toDouble();
+
+	if (pendingAdditiveOperator.isEmpty()){
+		sumSoFar = operand;
+	} else {
+		calculate(operand, pendingAdditiveOperator);
+		pendingAdditiveOperator.clear();
+	}
+
+	display->setText(QString::number(sumSoFar));
+	sumSoFar = 0.0;
+	waitingForOperand = true;
+}
+
 Button *Calculator::createButton(const QString &text, const char *member)
 {
 	Button *button = new Button(text);
 	connect(button, SIGNAL(clicked()), this, member);
 	return button;
+}
+
+bool Calculator::calculate(double operand, const QString &pendingOperator)
+{
+	if (pendingOperator == "+"){
+		sumSoFar += operand;
+	} else if (pendingOperator == "-"){
+		sumSoFar -= operand;
+	}
+	return true;
 }
